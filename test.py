ef4f1a43741a22686714129d4e49047e654b4d73
Michael <michael.r.jacobs@gmail.com>
2022-08-09 14:59:26 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,29 @@ port 			= "8443"
 baseUrl 		= f"https://gateway-{pin}.local:{port}/enduser-mobile-web/1/enduserAPI"
 token			= "62abc0d3efgh9b49d2e1"
     
-getapiversion 		= f"{baseUrl}/apiVersion"
-getdevices		= f"{baseUrl}/setup/devices"
-getsetup		= f"{baseUrl}/setup"
-getgateway		= f"{baseUrl}/setup/gateways"
+#Events    
+fetchevents 	    = f"{baseUrl}/events/:listenerId/fetch" #Post
+unregisterlistener  = f"{baseUrl}/events/:listenerId/unregister" #Post
+registerlistener    = f"{baseUrl}/events/register" #Post
+
+#Setup
+getdevice		    = f"{baseUrl}/setup/devices/:deviceURL" #Get
+getdevices		    = f"{baseUrl}/setup/devices" #Get
+getdevicescontrol 	= f"{baseUrl}/setup/devices/controllables/:controllableName" #Get
+getsetup		    = f"{baseUrl}/setup" #Get
+getgateway		    = f"{baseUrl}/setup/gateways" #Get
+
+#Exec
+cancelexecutions 	= f"{baseUrl}/exec/current/setup" #Del
+cancelspecificexec  = f"{baseUrl}/exec/current/setup/:executionId" #Del
+getexecutions       = f"{baseUrl}/exec/current" #Get
+getexecution	    = f"{baseUrl}/exec/current/:executionId" #Get
+executeaction       = f"{baseUrl}/exec/apply" #Post
+
+
+#Api version
+getapiversion 	    = f"{baseUrl}/apiVersion" #Get
+
 
 payload={}
 headers = {
@@ -25,19 +44,19 @@ headers = {
 #print(f"Headers = {headers}")
 
 import requests
+import json
 
 #Get verion
 #response = requests.request("GET", getapiversion, headers=headers, data=payload, verify=False)
 
 #Get devices
-# response = requests.request("GET", getdevices, headers=headers, data=payload, verify=False)
+response = requests.request("GET", getdevices, headers=headers, data=payload, verify=False)
+
 
 #Get setup
 #response = requests.request("GET", getsetup, headers=headers, data=payload, verify=False)
 
 #Get gateway
-response = requests.request("GET", getgateway, headers=headers, data=payload, verify=False)
+#response = requests.request("GET", getgateway, headers=headers, data=payload, verify=False)
 
-print(response.json())
-
-
+print (response.text)
